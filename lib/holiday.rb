e155d0d18439c9d4492f1473a86a08665301ddae
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-04-04 18:51:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,7 +24,7 @@ end
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :winter
-      holiday.each do |attribute, value|
+      holiday.each do |attribute, data|
         data << supply
       end
     end
